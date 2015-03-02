3a420d35a53a0cb1e5d3124ce5fccd553b9d1233
Wouter Bolsterlee <uws@xs4all.nl>
2015-03-02 14:03:41 +0100

    Add basic setup.py script

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+import jsonlines
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fp:
+    long_description = fp.read()
+
+setup(
+    name="jsonlines",
+    version=jsonlines.__version__,
+    description="Library with helpers for the jsonlines file format",
+    long_description=long_description,
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    url="https://github.com/wbolster/jsonlines",
+    packages=['jsonlines'],
+    license='BSD',
+    classifiers=[],  # TODO
+)
