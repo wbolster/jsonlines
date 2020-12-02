ee7d9ab06c89ecd654ee69b98105d674e79cd810
wouter bolsterlee <wouter@bolsterl.ee>
2020-12-02 18:25:31 +0100

    add dunder-all to avoid need for ‘noqa’ comment

--- jsonlines/__init__.py
+++ jsonlines/__init__.py
@@ -3,10 +3,18 @@ Module for the jsonlines data format.
 """
 
 # expose only public api
-from .jsonlines import (  # noqa
+from .jsonlines import (
     Reader,
     Writer,
     open,
     Error,
     InvalidLineError,
 )
+
+__all__ = [
+    "Reader",
+    "Writer",
+    "open",
+    "Error",
+    "InvalidLineError",
+]
